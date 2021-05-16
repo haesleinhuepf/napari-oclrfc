00a51d4371dda357f0edf574b471a7b4457937e7
haesleinhuepf <robert_haase@gmx.de>
2021-05-16 14:14:11 +0200

    flexibility in feature sets, code quality

--- napari_oclrfc/_function.py
+++ napari_oclrfc/_function.py
@@ -14,29 +14,31 @@ def napari_experimental_provide_function():
 
 import oclrfc
 
+
+class FeatureSets(Enum):
+    small = "original gaussian_blur=1 sobel_of_gaussian_blur=1 top_hat_box=5"
+    medium = "gaussian_blur=5 sobel_of_gaussian_blur=5 top_hat_box=25"
+    large = "gaussian_blur=25 sobel_of_gaussian_blur=25 top_hat_box=50"
+
 def train(
         image: "napari.types.ImageData",
         annotation : "napari.types.LabelsData",
         model_filename : str = "temp.cl",
+        featureset : FeatureSets = FeatureSets.small,
         max_depth : int = 2,
-        num_trees : int = 100
+        num_ensembles : int = 10
 ) -> "napari.types.LabelsData":
-    feature_stack = oclrfc.generate_feature_stack(image)
+    feature_stack = featureset.value
 
-    num_features = len(feature_stack)
-    num_classes = np.max(annotation)
+    clf = oclrfc.OCLRandomForestClassifier(opencl_filename=model_filename, num_ensembles=num_ensembles, max_depth=max_depth)
+    clf.train(feature_stack, annotation, image)
 
-    clf = oclrfc.OCLRandomForestClassifier(num_features, num_classes, opencl_filename=model_filename, num_trees=num_trees, max_depth=max_depth)
-    clf.train(feature_stack, annotation)
-
-    result = clf.predict_gpu(feature_stack)
+    result = clf.predict(feature_stack, image)
     return result
 
 def predict(image: "napari.types.ImageData",
         model_filename : str = "temp.cl") -> "napari.types.LabelsData":
 
-    feature_stack = oclrfc.generate_feature_stack(image)
-
-    clf = oclrfc.OCLRandomForestClassifier(0, 0, opencl_filename=model_filename)
-    result = clf.predict_gpu(feature_stack)
+    clf = oclrfc.OCLRandomForestClassifier(opencl_filename=model_filename)
+    result = clf.predict(image=image)
     return result
